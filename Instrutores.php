76f2203786f3ec2b753793a0701431583d73c252
Bárbara Ponciano <barbaraponciano13@gmail.com>
2014-07-04 12:26:38 -0400

    [ADJUST] modificando arquivos para php
    Signed-off-by: Bárbara Ponciano <barbaraponciano13@gmail.com>

---
+++ Instrutores.php
@@ -0,0 +1,43 @@
+<?php
+include "header.php";
+include "body.php";
+?>
+
+<div class="col-md-8">
+    <table class="table table-bordered table-hover" style="background-color: #8C8A8A;">
+        <thead>
+            <tr>
+                <th> Intrutores </th>
+                <th> Nome </th>
+                <th>Horário</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1º</td>
+                <td>Rafael</td>
+                <td>13 às 22</td>
+            </tr>
+
+            <tr>
+                <td>2º</td>
+                <td>Romulo</td>
+                <td>4 às 12</td>
+            </tr>
+
+            <tr>
+                <td>3º</td>
+                <td>Rodrigo</td>
+                <td>12 às 20</td>
+            </tr>
+
+            <tr>
+                <td>4º</td>
+                <td>Junior</td>
+                <td>4 às 12</td>
+            </tr>
+        </tbody>
+    </table>
+    <?php
+    include "footer.php";
+    
